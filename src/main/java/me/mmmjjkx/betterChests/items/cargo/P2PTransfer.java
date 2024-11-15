aa0ee38d5301c5d8dbd7811f845fda39b9426664
lijinhong11 <tygfhk@outlook.com>
2024-11-15 22:00:08 +0800

    fix: rm network components

--- src/main/java/me/mmmjjkx/betterChests/items/cargo/P2PTransfer.java
+++ src/main/java/me/mmmjjkx/betterChests/items/cargo/P2PTransfer.java
@@ -5,6 +5,7 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.handlers.BlockPlaceHandler;
 import me.mmmjjkx.betterChests.BCGroups;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
@@ -16,7 +17,8 @@ public class P2PTransfer extends SlimefunItem {
         addItemHandler(new BlockPlaceHandler(false) {
             @Override
             public void onPlayerPlace(@NotNull BlockPlaceEvent e) {
-
+                BlockFace f = e.getPlayer().getFacing();
+                BlockFace dest = f.getOppositeFace();
             }
         });
     }
